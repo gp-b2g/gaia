f5a82d24bc1457fc194a01360aa091c250320932
basiclines <igonzaleznicolas@gmail.com>
2013-05-23 12:21:56 +0200

    Added press&release support

--- shared/style/switches/module.js
+++ shared/style/switches/module.js
@@ -15,25 +15,31 @@ console.log("init");
 		var checkbox = rootElm[i];
 		var context = checkbox.parentNode;
 		var receiverElm = context.querySelector("span");
+
 		var isChecked = false;
+
+		// Amount of px moved by the user
+		var rawAmount = 0;
+
+		// Calculated from rawAmount, the backgorund position in px
 		var amount = 0;
+
+		// Px to determine non-intentional movements
+		var threshold = 3;
 
 		// limit = draggable area - handler width
 		var coordinates = { init: 0, current: 0, limit: 31 }
 
 		function move(e) {
 			coordinates.current = (e.touches) ? e.touches[0].pageX : e.clientX;
-			amount = coordinates.current - coordinates.init;
-
-				console.log(amount)
+			rawAmount = coordinates.current - coordinates.init;
 
 			if (isChecked && amount < 0) {
 				// Allow to move right to left
-				console.log("entra")
-
+				amount = rawAmount;
 			} else {
 				// Allow to move left to right
-				amount =  amount - coordinates.limit;
+				amount =  rawAmount - coordinates.limit;
 			}
 
 			receiverElm.style.backgroundPosition = amount+"px top";
@@ -41,7 +47,14 @@ console.log("init");
 
 		function start() {
 			console.log("press")
+
+			// Determine if the checkbox was checked before press
 			isChecked = checkbox.checked;
+
+			// Ensure that the initial amount is always 0
+			rawAmount = 0;
+			amount = 0;
+
 			// receiverElm.classList.remove("finish")
 			// receiverElm.classList.remove("moving")
 			context.addEventListener(moveEvent, move);
@@ -50,27 +63,35 @@ console.log("init");
 
 		function end() {
 			console.log("release")
-			var toCheck = (amount < (coordinates.limit / 2) * -1)
-			var finish = function() {
-				receiverElm.style.backgroundPosition = null;
-				receiverElm.classList.remove("finish");
-				// receiverElm.classList.add("moving");
-				receiverElm.removeEventListener("transitionend", finish);
-			};
 
-			if (isChecked && toCheck) {
-				// Uncheck
-				receiverElm.style.backgroundPosition = (coordinates.limit * -1) + "px top";
-				receiverElm.classList.add("finish");
-				checkbox.checked = false;
+			// true when user is just trying to press & release w/o movment
+			var isPressAndRelease = (rawAmount <= 0 + threshold && rawAmount >= 0 - threshold);
+			console.log(rawAmount)
+			if (!isPressAndRelease) {
+				// Intentional user movement
+				var toCheck = (amount < (coordinates.limit / 2) * -1)
+				var finish = function() {
+					receiverElm.style.backgroundPosition = null;
+					receiverElm.classList.remove("finish");
+					// receiverElm.classList.add("moving");
+					receiverElm.removeEventListener("transitionend", finish);
+				};
 
-				receiverElm.addEventListener("transitionend",  finish);
-			} else {
-				// Check
-				receiverElm.style.backgroundPosition = 0 + "px top";
-				receiverElm.classList.add("finish");
-				checkbox.checked = true;
-				receiverElm.addEventListener("transitionend",  finish);
+				// Determine if has to be checked or not
+				if (isChecked && toCheck) {
+					// Uncheck
+					receiverElm.style.backgroundPosition = (coordinates.limit * -1) + "px top";
+					receiverElm.classList.add("finish");
+					checkbox.checked = false;
+
+					receiverElm.addEventListener("transitionend",  finish);
+				} else {
+					// Check
+					receiverElm.style.backgroundPosition = 0 + "px top";
+					receiverElm.classList.add("finish");
+					checkbox.checked = true;
+					receiverElm.addEventListener("transitionend",  finish);
+				}
 			}
 
 			context.removeEventListener(moveEvent, move, false);
